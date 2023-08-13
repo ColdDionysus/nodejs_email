7df792cd50dc6620bbdc1e96987e9d3b879e1cfa
ColdDionysus <gautampukar02@gmail.com>
2023-08-13 17:52:01 +0545

    Sending Email

--- send_email.js
+++ send_email.js
@@ -1,27 +1,34 @@
-const nodemailer = require('nodemailer')
-require('dotenv').config()
+const nodemailer = require('nodemailer');
+
+const path = require('path'); // Import 'path' module for template path handling
+require('dotenv').config();
+
+// Configuring transporter
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
-        
         user: process.env.GMAIL_USERNAME,
         pass: process.env.GMAIL_PASS
     }
 });
-const main = async () => {
-    
+
+
+const main = async (receivers) => {
     const info = await transporter.sendMail({
-        from: '"Pukar Gautam 👻" <gautampukar01@gmail.com>', // sender address
-        to: "gautampukar01@gmail.com, gautampukar02@gmail.com", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
+        from: '"Pukar Gautam 👻" <gautampukar01@gmail.com>',
+        to: receivers.toString(),
+        subject: "Hello ✔",
+        template: "example", // Use the name of your template here
+        context: {
+            name: "Pukar Gautam"
+        }
     });
 
     console.log("Message sent: %s", info.messageId);
-    
 }
 
-main().catch(console.error);
+const receivers = ["gautampukar01@gmail.com", "gautampukar02@gmail.com"];
+
+main(receivers).catch(console.error);
